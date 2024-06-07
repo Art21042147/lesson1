06239f181ca1d995d8266513da98bd284126d056
Art21042147 <art21042147@gmail.com>
2024-06-07 09:21:23 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+a = 'string'
+print(len(a))
+first = 12
+second = 14
+summa = second + first
+print(summa)
+diff = first - second
+print(diff)
+first = 40
+second = 50
+third = 60
+mean = (first + second + third)/3
+print(mean)
+first_string = 'Monday'
+second_string = 'Tuesday'
+print(first_string + ", " + second_string)
+a = 23
+b = 12
+c = 8
+f = (a * b) + (a * c)
+print(f)
